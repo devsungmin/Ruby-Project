d16ac1828a843c85e42c7bc38fbbae284b0d577a
devsungmin <kimsungmin.dev@gmail.com>
2019-07-11 22:39:23 +0900

    주식 및 환률 정보

--- first_app/app/controllers/utilities_controller.rb
+++ first_app/app/controllers/utilities_controller.rb
@@ -3,9 +3,10 @@ class UtilitiesController < ApplicationController
   end
 
   def pick_lucky_numbers
-    unmbers = (1..45).to_a #to.a는 to array라는 줄임말로 1~45까지의 숫자가 출력 됨
-    random_numbers = unmbers.sample 6
-    @lucky_numbers = random_numbers.sort #오름차순으로 출력 됨
+    # unmbers = (1..45).to_a #to.a는 to array라는 줄임말로 1~45까지의 숫자가 출력 됨
+    # random_numbers = unmbers.sample 6
+    # @lucky_numbers = random_numbers.sort #오름차순으로 출력 됨
+    @lucky_numbers = (1..45).to_a.sample(6).sort
   end
 
   def get_stock_info
